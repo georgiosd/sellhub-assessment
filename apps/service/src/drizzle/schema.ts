7e919e586e09e2632d272c6474814139df712768
Georgios Diamantopoulos <georgiosd@gmail.com>
2024-12-31 19:13:20 +0200

    fix: make uuid primary key random

--- apps/service/src/drizzle/schema.ts
+++ apps/service/src/drizzle/schema.ts
@@ -3,7 +3,7 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import * as zod from "zod";
 
 export const products = pgTable("products", {
-  id: uuid("id").primaryKey(),
+  id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("name", { length: 255 }).notNull(),
   inventory_count: integer("inventory_count").notNull(),
 });
