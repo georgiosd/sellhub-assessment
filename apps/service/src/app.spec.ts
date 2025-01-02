e9456a7311528635fead9fc4f1da15d773822ea2
Georgios Diamantopoulos <georgiosd@gmail.com>
2025-01-02 11:33:08 +0200

    setup app tests with supertest

--- apps/service/src/app.spec.ts
+++ apps/service/src/app.spec.ts
@@ -1,9 +1,20 @@
-beforeEach(() => {
-  console.log("each");
+import { Express } from "express";
+import request from "supertest";
+import { createExpressApp } from "./app";
+
+let app: Express;
+
+beforeAll(async () => {
+  app = await createExpressApp({
+    connectionString: "postgres://postgres:password@localhost:5432/service",
+    isDevEnv: true,
+  });
 });
 
 describe("app", () => {
-  it("should run", () => {
-    console.log("run");
+  it("should return 200 on /", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.status).toBe(200);
   });
 });
