80b154545c064f7bb6e65f3639a8d25d872ee8c8
Georgios Diamantopoulos <georgiosd@gmail.com>
2025-01-02 10:50:22 +0200

    split into app file

---
+++ apps/service/src/app.ts
@@ -0,0 +1,51 @@
+import express, { Express, Request, Response } from "express";
+
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { reset, seed } from "drizzle-seed";
+import {
+  createDatabaseIfNotExistsAsync,
+  createDrizzleAsync,
+} from "./drizzle/db";
+import schema from "./drizzle/schema";
+
+function addRoutes(app: Express) {
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+  });
+
+  return app;
+}
+
+export async function createExpressApp({
+  connectionString,
+  isDevEnv,
+}: {
+  connectionString: string;
+  isDevEnv: boolean;
+}) {
+  const app: Express = addRoutes(express());
+
+  if (isDevEnv) {
+    await createDatabaseIfNotExistsAsync(connectionString);
+  }
+
+  const drizzle = await createDrizzleAsync(connectionString);
+
+  await migrate(drizzle, {
+    migrationsFolder: "./src/drizzle/migrations",
+  });
+
+  if (isDevEnv) {
+    await reset(drizzle, schema);
+
+    await seed(drizzle, schema).refine((f) => ({
+      products: {
+        columns: {
+          inventory_count: f.int({ minValue: 0 }),
+        },
+      },
+    }));
+  }
+
+  return app;
+}
--- apps/service/src/index.ts
+++ apps/service/src/index.ts
@@ -1,57 +1,7 @@
-import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
-import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { reset, seed } from "drizzle-seed";
-import {
-  createDatabaseIfNotExistsAsync,
-  createDrizzleAsync,
-} from "./drizzle/db";
-import schema from "./drizzle/schema";
+import { createExpressApp } from "./app";
 
 dotenv.config();
-
-function addRoutes(app: Express) {
-  app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
-  });
-
-  return app;
-}
-
-async function createExpressApp({
-  connectionString,
-  isDevEnv,
-}: {
-  connectionString: string;
-  isDevEnv: boolean;
-}) {
-  const app: Express = addRoutes(express());
-
-  if (isDevEnv) {
-    await createDatabaseIfNotExistsAsync(connectionString);
-  }
-
-  const drizzle = await createDrizzleAsync(connectionString);
-
-  await migrate(drizzle, {
-    migrationsFolder: "./src/drizzle/migrations",
-  });
-
-  if (isDevEnv) {
-    await reset(drizzle, schema);
-
-    await seed(drizzle, schema).refine((f) => ({
-      products: {
-        columns: {
-          inventory_count: f.int({ minValue: 0 }),
-        },
-      },
-    }));
-  }
-
-  return app;
-}
 
 async function main() {
   const app = await createExpressApp({
